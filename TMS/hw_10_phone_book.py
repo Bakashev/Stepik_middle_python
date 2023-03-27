f49e9f2173e9d7197c6e35e1d0eb0ac76eff5966
bakashev <bakashevs@mail.ru>
2023-03-27 14:37:34 +0300

    Added two method from class PhoneBook, Added method init from PhoneBook whith arg file_name

--- TMS/hw_10_phone_book.py
+++ TMS/hw_10_phone_book.py
@@ -4,16 +4,17 @@ import json
 # список контактов. Он должен иметь возможность искать контакты по имени и по телефону (два разных метода),
 # добавлять новые контакты и удалять контакты по имени или телефону. Контакты реализовать
 # в виде объектов класса Контакт. Данные телефонной книги хранить в json файле.
-
+file_name = 'my_contact.json'
 
 class PhoneBook:
-    file_name = 'my_contact.json'
-    with open(file_name, encoding='utf') as file:
+    def __init__(self, file_name: str):
+        self.file_name = file_name
+
+
+    with open(file_name, encoding='utf-8') as file:
         my_contact = json.load(file)
-    print(my_contact)
+        print(my_contact)
     print('---------------------')
-    def __init__(self):
-        pass
 
 
     def search_name(self, firstname: str) -> list:
@@ -26,13 +27,9 @@ class PhoneBook:
         return list(filter(lambda x: self.phone in x['phone'], PhoneBook.my_contact))
 
 
-    def write_json(self):
-        with open(PhoneBook.file_name, 'w', encoding='utf-8') as file:
+    def write_json(self, file_name):
+        with open(file_name, 'w', encoding='utf-8') as file:
             json.dump(PhoneBook.my_contact, file, indent=4)
-
-
-class Contact(PhoneBook):
-
 
     def add_new_contat(self, firstname, phone):
         self.firstname = firstname
@@ -41,7 +38,7 @@ class Contact(PhoneBook):
                             PhoneBook.my_contact))):
             new_contact = {'name': self.firstname, 'phone': self.phone}
             PhoneBook.my_contact.append(new_contact)
-            super().write_json()
+            PhoneBook.write_json(self, file_name)
 
 
     def del_contact(self, firstname='', phone='') -> None:
@@ -52,19 +49,28 @@ class Contact(PhoneBook):
                 answer = input(f'Удалить текущий контакт {value} y/n: ')
                 if answer == 'y':
                     PhoneBook.my_contact.pop(index)
-            super().write_json()
+            PhoneBook.write_json(self, file_name)
+
+class Contact():
+    def __init__(self, firstname, phone):
+        self.firstname = firstname
+        self.phone = phone
 
 
 
-contact = PhoneBook()
+contact1 = Contact('Stephan', '+2342352352')
+contact = PhoneBook(file_name)
+contact.add_new_contat(contact1.firstname, contact1.phone)
 name = input('Enter name for search: ')
 print(contact.search_name(name))
 print('--------------------')
 phone = input('Enter phone for search: ')
 print(contact.serch_phone(phone))
 print('-------------------')
-contact1 = Contact()
-contact1.add_new_contat('Lesha1', '+3214121412411')
-contact1.del_contact(phone='+3214121412411')
-contact1.del_contact(firstname='Serhei')
+
+contact.del_contact(firstname='Lesha1')
+contact.del_contact(phone='+2342352352')
+# contact1.add_new_contat('Lesha1', '+3214121412411')
+# contact1.del_contact(phone='+3214121412411')
+# contact1.del_contact(firstname='Serhei')
 print(PhoneBook.my_contact)
